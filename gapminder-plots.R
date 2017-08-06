716d24fc6f71943bc71aec114fdea0687c12058f
Katrina Hounchell <hounchel@stanford.edu>
2017-08-06 10:02:08 -0700

    add scatter plot

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -1,2 +1,11 @@
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv")
 gapminder <- read.csv ("data/gapminder-FiveYears.csv")
+
+library(ggplot2)
+ggplot(gapminder_FiveYearData, aes(x = gdpPercap, y = lifeExp)) +
+  geom_point()
+
+gapminder <- gapminder_FiveYearData
+
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) +
+  geom_point()
