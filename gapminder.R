d7c722793be3f0779b60a69afaf2297ae25487d3
Katrina Hounchell <hounchel@stanford.edu>
2017-08-06 09:47:34 -0700

    add code to read gapminder data

---
+++ gapminder.R
@@ -0,0 +1,2 @@
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv")
+gapminder <- read.csv ("data/gapminder-FiveYears.csv")
